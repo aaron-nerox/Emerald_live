82f77941805fdd22894fcf980b4b1454bc815547
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-08-31 01:57:06 +0100

    add: added the about section

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <Main />
+  <Home />
 </template>
 
 <script>
-import Main from "./views/Main.vue";
+import Home from "./views/Home.vue";
 
 export default {
   name: "App",
   components: {
-    Main,
+    Home,
   },
 };
 </script>
@@ -24,11 +24,35 @@ export default {
   src: url("./assets/fonts/Modulus-Bold.otf");
 }
 
+html{
+  scroll-behavior: smooth;
+}
+
 #app {
+  min-height: 100vh;
   background-image: url("./assets/images/background.svg");
   background-repeat: no-repeat;
   background-size: cover;
-  min-height: 100vh;
+  display: grid;
+}
+
+::-webkit-scrollbar{
+  width: 12px;
+}
+
+::-webkit-scrollbar-track-piece{
+  background-color: #fff;
+  
+}
+
+::-webkit-scrollbar-thumb{
+  background: rgb(137, 237, 255);
+  border-radius: 100px;
+  transition: 300ms ease-in;
+}
+
+::-webkit-scrollbar-thumb:hover{
+  background: rgb(0, 217, 255);
 }
 
 .h1title{
@@ -44,10 +68,19 @@ export default {
 .subtitle{
   font-family: modulus_bold;
   font-size: 1.4em;
+  line-height: 1.5;
 }
 
 .text{
   font-family: modulus;
   font-size: 1.1em;
+}
+
+.x-card{
+  background-color: white;
+  border-radius: 20px;
+  margin: 20px;
+  padding: 15px;
+  color: #222;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <x-header></x-header>
+  <section id="about" class="x-card section-container">
+    <div class="inner-section-container">
+      <img src="../assets/images/logo_image.svg" alt="visual logo" class="about-logo">
+      <div class="text-container">
+        <p class="title">About the system:</p>
+        <p class="subtitle">The X design system came from the need to 
+          have a system that is familiar to both dominant mobile 
+          platforms "android" and "ios", it is a complete mobile ui
+          , designed to be simple, diverse and all inclusive. </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import XHeader from "../components/Header.vue";
+
+export default {
+  name: "Home",
+  components: {
+    XHeader,
+  },
+};
+</script>
+
+<style scoped>
+.section-container{
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.inner-section-container{
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.about-logo{
+  width: 18%;
+  border-radius: 20px;
+  margin-right: 15px;
+}
+
+.text-container{
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.title{
+  margin: 0px;
+}
+
+</style>
